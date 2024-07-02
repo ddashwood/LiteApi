b51f6a618d44fc62ef7c1a486035b37acecea95c
Dean Dashwood <dean@dashwood.world>
2024-07-02 15:48:44 +0100

    Remove service registration for class which is now created with ActivatorUtilities.CreateInstance

--- LiteApi/LiteApiServerExtensions.cs
+++ LiteApi/LiteApiServerExtensions.cs
@@ -7,7 +7,6 @@ public static class LiteApiServerExtensions
 {
     public static IServiceCollection AddLiteApi(this IServiceCollection services, int port, Action<MiddlewarePipelineConfiguration> config)
     {
-        services.AddScoped<FileServerMiddleware>();
         services.AddHostedService(services => ActivatorUtilities.CreateInstance<LiteApiServer>(services, port, config));
         return services;
     }
